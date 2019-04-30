d9077091eab2ee26688c2b1b2bbb61ac5c815420
ayshiff <remi.d45@gmail.com>
2019-04-30 13:50:11 +0200

    feat: add list of tools config

---
+++ babel.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        modules: false,
+      },
+    ],
+    '@babel/preset-react',
+  ],
+  plugins: [
+    'styled-components',
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-syntax-dynamic-import',
+    'transform-async-to-generator'
+  ],
+  env: {
+    production: {
+      only: ['app'],
+      plugins: [
+        'lodash',
+        'transform-react-remove-prop-types',
+        '@babel/plugin-transform-react-inline-elements',
+        '@babel/plugin-transform-react-constant-elements',
+        'transform-async-to-generator'
+      ],
+    },
+    test: {
+      plugins: [
+        '@babel/plugin-transform-modules-commonjs',
+        'dynamic-import-node',
+        '@babel/plugin-transform-runtime',
+      ],
+    },
+  },
+};
