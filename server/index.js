650d30d91fcd3e59e5fc708711fa07aa967a9c96
sundowndev <raphael@crvx.fr>
2019-05-02 18:54:23 +0200

    refactor(server): db connect

--- server/index.js
+++ server/index.js
@@ -3,11 +3,13 @@ import helmet from 'helmet';
 import express from 'express';
 import bodyParser from 'body-parser';
 import http from 'http';
+import logger from 'morgan';
 
 // Internals libraries
 // import initializeExpressArgument from '@/common/initialize-express-argument';
 import api_response from './app/response/api_response';
 import api_response_error from './app/response/api_error_response';
+import { connect } from '@/db/db.connect';
 
 // Routes
 import products from './app/routes/products';
@@ -19,11 +21,13 @@ const app = express();
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-// app.use(queryParser());
+connect();
 
 // ---------- CONFIGURATION HEADERS HTTP ----------
 // app.use(config_headers);
 // app.use(mandatory_headers);
+
+app.use(logger('[:method] :url :status - :response-time ms'));
 
 // ROUTES
 app.use('/products', products);
