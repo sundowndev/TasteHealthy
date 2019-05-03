9884b1d57cf071f33bc28b3acda59ee4e91bcd71
sundowndev <raphael@crvx.fr>
2019-05-03 10:17:33 +0200

    feat(server): setup tests

---
+++ server/test/schemas/products.spec.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-console */
+
+// import { connect, client } from '../../../app/db/db.connect';
+
+describe('PRODUCTS ROUTES -- GET', () => {
+  beforeAll(() => {
+    console.log('before all');
+  });
+
+  afterAll(() => {
+    console.log('after all');
+  });
+
+  it('should succeed', () => {
+    expect(1).toEqual(1);
+  });
+});
