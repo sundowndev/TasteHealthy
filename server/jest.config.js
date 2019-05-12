94f09cf7eb061c7f2b70947e3adde03a3d999f8a
sundowndev <raphael@crvx.fr>
2019-05-12 18:50:39 +0200

    refactor(api): jest config

--- server/jest.config.js
+++ server/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  collectCoverage: true,
+  coverageReporters: ['json', 'html'],
+  coverageDirectory: './server/coverage',
   moduleDirectories: ['node_modules'],
   testRegex: '.*\\/.*\\.spec\\.js$',
   snapshotSerializers: [],
