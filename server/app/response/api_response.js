1f431ce21209825124b5bd6509601767036df52c
sundowndev <raphael@crvx.fr>
2019-05-20 14:20:41 +0200

    refactor(api): return options in response

--- server/app/response/api_response.js
+++ server/app/response/api_response.js
@@ -1,20 +1,23 @@
 export default function(req, res, next) {
   if (!res.finished) {
     res.set('Content-Type', 'application/json; charset=utf-8');
-    if (!req.route) res.sendStatus(404);
-    else {
+    if (!req.route) {
+      res.status(404);
+      res.json({ success: false, message: 'Route not found.' });
+    } else {
       let json = { success: true };
 
       if (req.results) {
         json.results = req.results;
       }
 
+      if (req.opts_return) {
+        json = Object.assign(json, req.opts_return);
+      }
+
       if (req.return) {
         if (Array.isArray(req.return)) json.items = req.return;
         else json.item = req.return;
-      }
-      if (req.opts_return) {
-        json = Object.assign(json, req.opts_return);
       }
 
       res.status(req.status || 200);
