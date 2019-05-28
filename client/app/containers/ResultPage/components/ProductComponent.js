f5cb26092b40a78c0d09033b1759cb15ea1ea6df
ayshiff <remi.d45@gmail.com>
2019-05-28 18:21:05 +0200

    feat: add ResultPage components

---
+++ client/app/containers/ResultPage/components/ProductComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../styles/ResultPage.css';
+
+const ProductComponent = () => (
+  <div className="app__content__substitute-products__block">
+    <div>
+      <strong>Riz Blanc</strong>
+      <p>100g</p>
+    </div>
+    <div className="app__content__substitute-products__block__check" />
+  </div>
+);
+
+export default ProductComponent;
---
+++ client/app/containers/ResultPage/components/SubstituteProductComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../../styles/ResultPage.css';
+
+const SubstituteProductComponent = () => (
+  <div className="app__content__substitute-products__block">
+    <div className="app__content__substitute-products__block__check is-checked" />
+    <div>
+      <strong>Riz Blanc</strong>
+      <p>100g</p>
+    </div>
+  </div>
+);
+
+export default SubstituteProductComponent;
