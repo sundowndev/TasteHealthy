d171a79ebd1161b0811fc942d3e18e99a7b3d6e5
ayshiff <remi.d45@gmail.com>
2019-05-28 18:23:27 +0200

    feat: refactoring MealsPage

--- client/app/containers/MealsPage/index.js
+++ client/app/containers/MealsPage/index.js
@@ -15,6 +15,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import Modal from 'react-modal';
 import axios from 'axios';
+import ModalComponent from './components/ModalComponent';
 // import { withRouter } from 'react-router';
 import messages from './messages';
 
@@ -38,6 +39,9 @@ type propsType = {
   changeSnack: mealsType => any,
   changeDinner: mealsType => any,
   mealsData: mealsType,
+  history: {
+    push: string => void,
+  },
 };
 
 const customStyles = {
@@ -47,114 +51,6 @@ const customStyles = {
     right: '5%',
     bottom: '5%',
   },
-};
-
-const SearchBar = () => {
-  const [valueSearch, handleChange] = useState('');
-
-  const handleValueChange = (name: string) => {
-    handleChange(name);
-
-    if (name.length >= 3) {
-      axios
-        .post(`localhost:3000/products?query=${name}`)
-        .then(data => console.log(data));
-    }
-  };
-
-  return (
-    <input
-      type="text"
-      value={valueSearch}
-      onChange={e => handleValueChange(e.target.value)}
-      placeholder="Search..."
-    />
-  );
-};
-
-const ModalComponent = ({ mealsData }: { mealsData: mealsType }) => {
-  const [consummedAliments, changeConsumedAliments] = useState([]);
-  const [meals, setMeals] = useState(mealsData);
-  return (
-    <div>
-      <SearchBar />
-      {[
-        { name: 'ZAEAZZE', id: 132, quantity: 100 },
-        { name: 'fsddsfsd', id: 341, quantity: 100 },
-      ].map(_ => (
-        // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-        // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-        <div
-          onClick={() => {
-            if (!consummedAliments.some(e => e.id === _.id)) {
-              changeConsumedAliments(consummedAliments.concat([_]));
-            }
-          }}
-          key={_.id}
-        >
-          {_.name}
-        </div>
-      ))}
-      <button
-        type="button"
-        onClick={() =>
-          setMeals({
-            ...meals,
-            breakfast: {
-              test: 'OK',
-            },
-          })
-        }
-      >
-        Valider
-      </button>
-      <h1>Aliments consommés</h1>
-      {consummedAliments.map(el => (
-        // eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions
-        <div>
-          <h1
-            key={el.id}
-            onClick={() =>
-              changeConsumedAliments(
-                consummedAliments.filter(ll => ll.id !== el.id),
-              )
-            }
-          >
-            {el.name}
-            {el.quantity} gr
-          </h1>
-          <p
-            onClick={() => {
-              const tt = consummedAliments.map(aliment => {
-                if (aliment.id === el.id) {
-                  if (el.quantity > 100) {
-                    aliment.quantity -= 100;
-                  }
-                }
-                return aliment;
-              });
-              changeConsumedAliments(tt);
-            }}
-          >
-            -
-          </p>
-          <p
-            onClick={() => {
-              const tt = consummedAliments.map(aliment => {
-                if (aliment.id === el.id) {
-                  aliment.quantity += 100;
-                }
-                return aliment;
-              });
-              changeConsumedAliments(tt);
-            }}
-          >
-            +
-          </p>
-        </div>
-      ))}
-    </div>
-  );
 };
 
 export const HomePage = (props: propsType) => {
@@ -189,21 +85,21 @@ export const HomePage = (props: propsType) => {
           close
         </button>
         <div>{currentModalName}</div>
-        <ModalComponent mealsData={props.mealsData} />
+        <ModalComponent props={props} currentModalName={currentModalName} />
       </Modal>
       {/**  */}
 
       <FormattedMessage {...messages.header} />
-      <button type="button" onClick={() => openModal('Breakfast')}>
+      <button type="button" onClick={() => openModal('breakfast')}>
         Breakfast
       </button>
-      <button type="button" onClick={() => openModal('Lunch')}>
+      <button type="button" onClick={() => openModal('lunch')}>
         Lunch
       </button>
-      <button type="button" onClick={() => openModal('Dinner')}>
+      <button type="button" onClick={() => openModal('dinner')}>
         Dinner
       </button>
-      <button type="button" onClick={() => openModal('Snack')}>
+      <button type="button" onClick={() => openModal('snack')}>
         Snack
       </button>
     </h1>
