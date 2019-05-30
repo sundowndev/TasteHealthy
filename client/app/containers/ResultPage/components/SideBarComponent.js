7695490cbbd6aef47098f84b99a608626f3bc7bd
ayshiff <remi.d45@gmail.com>
2019-05-30 17:35:32 +0200

    feat: update dataviz container

--- client/app/containers/ResultPage/components/SideBarComponent.js
+++ client/app/containers/ResultPage/components/SideBarComponent.js
@@ -1,39 +1,70 @@
 import React from 'react';
 import '../../../styles/ResultPage.css';
 
-const SidebarComponent = () => (
-  <div className="app__sidebar">
-    <p className="app__sidebar__title">Ma consommation</p>
-    <div className="app__sidebar__content">
-      <div className="app__sidebar__content__block">
-        <div className="app__sidebar__content__block__left">
-          <p>Petit Déjeuner</p>
+const SidebarComponent = ({ props }: { props: any }) => {
+  console.log(props);
+  return (
+    <div className="app__sidebar">
+      <p className="app__sidebar__title">Ma consommation</p>
+      <div className="app__sidebar__content">
+        <div
+          onClick={() =>
+            props.mealsData.breakfast.consummedAliments
+              ? props.history.push('/result/breakfast')
+              : null
+          }
+          className="app__sidebar__content__block"
+        >
+          <div className="app__sidebar__content__block__left">
+            <p>Petit Déjeuner</p>
+          </div>
+          <div className="app__sidebar__content__block__right" />
         </div>
-        <div className="app__sidebar__content__block__right" />
-      </div>
 
-      <div className="app__sidebar__content__block">
-        <div className="app__sidebar__content__block__left">
-          <p>Déjeuner</p>
+        <div
+          onClick={() =>
+            props.mealsData.lunch.consummedAliments
+              ? props.history.push('/result/lunch')
+              : null
+          }
+          className="app__sidebar__content__block"
+        >
+          <div className="app__sidebar__content__block__left">
+            <p>Déjeuner</p>
+          </div>
+          <div className="app__sidebar__content__block__right" />
         </div>
-        <div className="app__sidebar__content__block__right" />
-      </div>
 
-      <div className="app__sidebar__content__block">
-        <div className="app__sidebar__content__block__left">
-          <p>Goûter</p>
+        <div
+          onClick={() =>
+            props.mealsData.snack.consummedAliments
+              ? props.history.push('/result/snack')
+              : null
+          }
+          className="app__sidebar__content__block"
+        >
+          <div className="app__sidebar__content__block__left">
+            <p>Goûter</p>
+          </div>
+          <div className="app__sidebar__content__block__right" />
         </div>
-        <div className="app__sidebar__content__block__right" />
-      </div>
 
-      <div className="app__sidebar__content__block">
-        <div className="app__sidebar__content__block__left">
-          <p>Dinner</p>
+        <div
+          onClick={() =>
+            props.mealsData.breakfast.consummedAliments
+              ? props.history.push('/result/dinner')
+              : null
+          }
+          className="app__sidebar__content__block"
+        >
+          <div className="app__sidebar__content__block__left">
+            <p>Dinner</p>
+          </div>
+          <div className="app__sidebar__content__block__right" />
         </div>
-        <div className="app__sidebar__content__block__right" />
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default SidebarComponent;
